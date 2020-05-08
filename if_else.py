42c23c8235666de5cf697718a2cda7fb7758d29b
SUMYAK JAIN <3456gaurav@gmail.com>
2020-05-08 17:17:00 +0530

    Python If-Else

---
+++ if_else.py
@@ -0,0 +1,9 @@
+a=int(input())
+if a%2==0 and a>2 and a<5:
+    print("Not Weird")
+elif a%2==0 and a>6 and a<20:
+    print("Weird")
+elif a%2==0 and a>20:
+    print("Not Weird")
+else:
+    print("Weird")
